19d5cec4da5086d9286efa7adfd50666f795d9c2
vabene1111 <vabene1111@users.noreply.github.com>
2019-03-28 17:26:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ set -e
 echo "#########################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
+# Make sure settings file is readable
+chmod -R 777 .
+
+# Override database settings to sqlite db
+SETTINGS="($find . -name settings.py)"
+echo "DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}" >> "$SETTINGS"
+
+# run test
 sh -c "python manage.py test"
 
 echo "#########################################"
